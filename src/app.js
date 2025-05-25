1649ad78f2ac2fc461da4a5a0901e38c5ed2cc8a
Raj Kumar Pal <palrajkumar999@gmail.com>
2025-05-25 22:44:31 +0530

    git first

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res)=>{
+    res.send("Servers are Ready");
+});
+
+app.get("/hello",(req,res)=>{
+    res.send("Helllo Helllo");
+});
+
+app.get("/test",(req,res)=>{
+    res.send("Testing ROutes Call");
+});
+
+app.listen(3000,()=>{
+    console.log(`Server Listen Port ${3000}`);
+});
