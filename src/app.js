1282ff77402f2df1de3abf43c5d851a13970b7ce
Raj Kumar Pal <palrajkumar999@gmail.com>
2025-05-25 22:50:25 +0530

    changes app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/",(req,res)=>{
-    res.send("Servers are Ready");
+    res.send("Servers are Readys");
 });
 
 app.get("/hello",(req,res)=>{
