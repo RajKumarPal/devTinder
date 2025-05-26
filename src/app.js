ef74854ee144b64f3040fa2788914b19e8187fb9
Raj Kumar Pal <palrajkumar999@gmail.com>
2025-05-26 11:11:14 +0530

    sdffsd

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Servers are Readys");
-});
-
 app.get("/hello",(req,res)=>{
     res.send("Helllo Helllo");
 });
 
 app.get("/test",(req,res)=>{
-    res.send("Testing ROutes Call");
+    res.send("Testing Routes Call");
+});
+
+app.use("/",(req,res)=>{
+    res.send("Servers are Readys");
 });
 
 app.listen(3000,()=>{
